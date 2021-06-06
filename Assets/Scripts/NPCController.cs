5b68ebbc39b25dcddc2bf882edd99974719c94bd
Ersan Tzampaz Chatip <hatipersan@gmail.com>
2021-06-06 10:06:40 +0300

    Added interaction with npc functionality. Used Interface to do it. added a test npc

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour, Interactable
+{
+    public void Interact()
+    {
+        Debug.Log("Interact with npc");
+    }
+
+}
